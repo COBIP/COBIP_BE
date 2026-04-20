935b86ddafcd34e71322f8a3bb6a4da2cb3b6cab
dev-burnern <dev-burnern@users.noreply.github.com>
2026-04-20 20:41:41 +0900

    chore: 프로젝트 초기 골격 구성

--- README.md
+++
@@ -1,1 +0,0 @@
-# COBIP_BE
---
+++ src/main/java/com/cobip/CobipApplication.java
@@ -0,0 +1,13 @@
+package com.cobip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobipApplication.class, args);
+	}
+
+}
