e000a622aa9d5cbdf92918b340223b2c06ac1290
burnern <200775118+dev-burnern@users.noreply.github.com>
2026-05-09 14:53:47 +0900

    feat(auth): 이메일 닉네임 중복 확인 API 추가

--- src/main/java/com/cobip/global/config/SecurityConfig.java
+++ src/main/java/com/cobip/global/config/SecurityConfig.java
@@ -46,6 +46,8 @@ public class SecurityConfig {
                                 "/api/v1/auth/signup",
                                 "/api/v1/auth/login",
                                 "/api/v1/auth/reissue",
+                                "/api/v1/auth/email/availability",
+                                "/api/v1/auth/nickname/availability",
                                 "/api/v1/auth/email-verifications",
                                 "/api/v1/auth/email-verifications/confirm"
                         ).permitAll()
